a42b3033453358148b16d8fe6270befccb12c5c0
claudomiro <claudomiromonteiro@gmail.com>
2023-01-06 12:08:08 -0300

    update lib/axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://expenses-back-end.vercel.app/',
+  // baseURL: 'http://localhost:3333',
 })
